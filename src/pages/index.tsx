e0aaaacf13b3abfac16d44202c84bdfd75cc042f
andreteixeira98 <andreteixeiracbjr@gmail.com>
2022-09-30 18:40:23 -0300

    feat: initial

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Head from 'next/head';
+
+export default function Home() {
+  return [
+    
+    <Head key={1}>
+       <title> Inicio | ig.news </title>
+    </Head>,
+    
+    <h1 key={2}>
+     Hello world
+    </h1>
+  ]
+}
